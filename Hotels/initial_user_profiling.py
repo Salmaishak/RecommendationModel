73557610742bb00d7f518f6d9fbdc942242e24da
Salma Magdy Ishak <salma.m.ishakk@gmail.com>
2023-03-28 15:50:12 +0200

    update on user profiling

--- Hotels/initial_user_profiling.py
+++ Hotels/initial_user_profiling.py
@@ -7,7 +7,7 @@ def intersection(lst1, lst2, total_num):
     return ((len(lst3)/ total_num) * 5)
 
 def user_profile():
-    allhotels = pd.read_csv('Allhotels.csv')
+    allhotels = pd.read_csv('Allhotels.csv' ,encoding='latin-1')
 
     amenities = {1: 'Restaurant', 2: 'Air conditioning', 3: 'Laundry service', 4:'Room service', 5:'Airport transportation',6:'Non-smoking rooms',
                  7:'24-hour front desk',8:'Bar / lounge', 9:'Family rooms', 10:'Safe', 11:'Wifi',12:'Pool',13:'Dry cleaning',
@@ -16,11 +16,12 @@ def user_profile():
     preferred_amenities = []
     user_input = ''
     total_num = 0
-    while user_input != '0':
-        num = int(input("Please choose numbers of your favorite amenities (type '0' to exit): "))
-        print(amenities[num])
-        preferred_amenities.append(amenities[num])
-        total_num = total_num + 1
+    while user_input != 0:
+        user_input = int(input("Please choose numbers of your favorite amenities (type '0' to exit): "))
+        if user_input!=0:
+            print(amenities[user_input])
+            preferred_amenities.append(amenities[user_input])
+            total_num = total_num + 1
 
 
 
@@ -31,30 +32,22 @@ def user_profile():
 
 
        if (pd.isna(row[4])):
-
-            user_rating_df.append(
-                {
-                    'user_id' : user_id,
-                    'hotel_id' : row[0],
-                    'rating' : int(0),
-                    'flag': 'F'
-
-                }
-            )
             continue
-
        amenities = row[4]
        row_amenities = amenities.split(",")
        #print(row_amenities)
-       user_rating_df.append(
-           {
-               'user_id': user_id,
-               'hotel_id': row[0],
-               'rating': intersection(row_amenities, preferred_amenities),
-               'flag': 'F'
+       interValue = intersection(row_amenities, preferred_amenities,total_num)
+       if (interValue > 3):
+           user_rating_df.append(
+               {
+                   'userID': user_id,
+                   'hotelID': row[0],
+                   'ratings': interValue,
+                   'flag': 'F',
+                   'city': row[7]
 
-           }
-       )
+               }
+           )
 
     print(pd.DataFrame(user_rating_df))
     pd.DataFrame(user_rating_df).to_csv('user_profiling.csv', index = False)
--- test_env.py
+++ test_env.py
@@ -2,8 +2,8 @@ from Hotels.Hotels_MF_ALS import *
 import pandas as pd
 
 #TEST CODE FOR HOTELS
-csvHotelInfo = "Allhotels.csv"
-csvRatingInfo = 'user_profiling30.csv'
+csvHotelInfo = "Hotels/Allhotels.csv"
+csvRatingInfo = 'Hotels/user_profiling.csv'
 
 newUser = []
 
@@ -16,15 +16,15 @@ newUser.append(
                    'city':'Cairo'
 
                })
-pd.DataFrame(newUser).to_csv('Hotels/user_profile_Data/user_profiling30.csv', index=False, mode='a', header=False)
+pd.DataFrame(newUser).to_csv('Hotels/user_profile_Data/user_profiling_AlFay.csv', index=False, mode='a', header=False)
 #when entering a new user we do have to retrain the entire model
 ratingsSpark,hotels = initial_files(csvHotelInfo, csvRatingInfo)
 calculateSparsity(ratingsSpark)
 train,test= dataSplit(ratingsSpark)
 best_model =MF_ALS(train,test)
 
-recommendations(best_model,2709,hotels,'Giza')
+recommendations(best_model,1,hotels,'Al-Fayyum')
 
 #TEST CODE FOR RESTAURANTS
 
-#TEST CODE FOR ATTRACTIONS 
+#TEST CODE FOR ATTRACTIONS
