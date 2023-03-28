6b7eb4bcd2001624f07c8eba380c426d81f81508
RawanGamalElDin12 <rawan.gamaleldin@outlook.com>
2023-03-28 21:11:11 +0200

    Restaurants User Profiling

---
+++ Restaurants/restaurants_user_profiling.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+
+
+def intersection(lst1, lst2, total_num):
+    lst3 = [value for value in lst1 if value in lst2]
+    return ((len(lst3)/ total_num) * 5)
+
+def user_profile():
+    allRestaurants = pd.read_csv('C:\\Users\\rawan\\Desktop\\RecommendationModel\\Restaurants\\Cairo_Final_Clean_Updated.csv' ,encoding='latin-1')
+
+    cuisine_types = {1: 'Mediterranean', 2: 'Egyptian', 3: 'Italian', 4:'Seafood', 5:'Middle Eastern',6:'European',
+                 7:'American',8:'Vegetarian Friendly', 9:'Lebanese', 10:'Barbecue', 11:'Japanese',12:'Healthy',13:'Steakhouse',
+                 14:'French', 15:'International'}
+
+    preferred_cuisine_types = []
+    user_input = ''
+    total_num = 0
+    while user_input != 0:
+        user_input = int(input("Please choose numbers of your favorite cuisine_types (type '0' to exit): "))
+        if user_input!=0:
+            print(cuisine_types[user_input])
+            preferred_cuisine_types.append(cuisine_types[user_input])
+            total_num = total_num + 1
+
+
+
+    user_id = 1
+    user_rating_df = []
+    print(preferred_cuisine_types)
+    for ind, row in allRestaurants.iterrows():
+
+
+       if (pd.isna(row[7])):
+            continue
+       cuisine_types = row[7]
+       row_cuisine_types = cuisine_types.split(",")
+
+       interValue = intersection(row_cuisine_types, preferred_cuisine_types,total_num)
+       if (interValue > 3):
+           user_rating_df.append(
+               {
+                   'userID': user_id,
+                   'restID': row[0],
+                   'ratings': interValue,
+                   'flag': 'F',
+                   'city': row[2]
+
+               }
+           )
+
+    print(pd.DataFrame(user_rating_df))
+    pd.DataFrame(user_rating_df).to_csv('user_profiling_rest.csv', index = False)
+
+user_profile()
