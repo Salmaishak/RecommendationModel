d0eeade50ff4c6df69fa58811d86bfcedca0c74e
Nancy Ahmed <85074384+Nancyy20@users.noreply.github.com>
2023-04-07 02:43:05 +0200

    Add files via upload

--- attractions_reccommendation/profiling_new_user.py
+++ attractions_reccommendation/profiling_new_user.py
@@ -1,14 +1,53 @@
-def profiling_new_user(user_id, attraction_id, rating, city):
-    with open("user_profiling3010.csv", 'a') as file:
-        file.write("\n")
-        file.write(user_id)
-        file.write(",")
-        file.write(attraction_id)
-        file.write(",")
-        file.write(rating)
-        file.write(",")
-        file.write('F')
-        file.write(",")
-        file.write(city)
+import pandas as pd
 
-# profiling_new_user('31','205','4','cairo')
+
+def intersection(lst1, lst2):
+    if lst1 in lst2:
+        return True
+    else:
+        return False
+
+
+def profiling_new_user(user_id, preferences):
+    attractions = pd.read_csv('attractions.csv', encoding='latin-1')
+
+    attraction_types = {1: 'Museum', 2: 'Park', 3: 'Historical landmark', 4: 'Beach', 5: 'Garden', 6: 'Art Gallery',
+                        7: 'Palace', 8: 'Mosque', 9: 'Church', 10: 'Shopping mall', 11: 'Temple', 12: 'Bazar',
+                        13: 'island', 14: 'Zoo', 15: 'Library'}
+
+    preferred_types = []
+
+    for i in preferences:
+        preferred_types.append(attraction_types[i])
+        print(attraction_types[i])
+
+    user_rating_df = []
+    print(preferred_types)
+
+    for ind, col in attractions.iterrows():
+        if pd.isna(col[4]):
+            continue
+        attraction_types = col[4]
+        # row_attractions = attraction_types.split(",")
+        # print(row_attractions)
+
+        if intersection(attraction_types, preferred_types):
+            rating = 5
+        else:
+            rating = int(1)
+        if rating > 1:
+            user_rating_df.append(
+                {
+                    'user_id': user_id,
+                    'attraction_id': col[0],
+                    'rating': rating,
+                    'flag': 'F',
+                    'city': col[8]
+
+                }
+            )
+
+    pd.DataFrame(user_rating_df).to_csv('user_profiling3010.csv', index=False, mode='a', header=False)
+
+
+# profiling_new_user(31, [1,3])
